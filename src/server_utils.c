ded88e5808395cd70b5a6ce322a2d863225aabd4
Ayoub IDEL <ayoub.idel@hotmail.com>
2019-03-01 14:37:12 +0100

    add server_utils.c/.h (included in queue)

---
+++ src/server_utils.c
@@ -0,0 +1,13 @@
+//
+// Created by Ayoub IDEL  on 25/02/2019.
+//
+
+#include "server_utils.h"
+
+
+/**
+ * //TODO : Server side
+ *
+ * Server is included in the files Queue.h/.c
+ *
+ */
---
+++ src/server_utils.h
@@ -0,0 +1,16 @@
+//
+// Created by Ayoub IDEL  on 25/02/2019.
+//
+
+#ifndef PRD_CLEAN_SERVER_UTILS_H
+#define PRD_CLEAN_SERVER_UTILS_H
+
+
+/**
+ * //TODO : Server side
+ *
+ * Server is included in the files Queue.h/.c
+ *
+ */
+
+#endif //PRD_CLEAN_SERVER_UTILS_H
